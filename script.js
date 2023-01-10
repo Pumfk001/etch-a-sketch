af380d63c66af9d8705b31120290580fb4929d46
Pumfk001 <110676191+Pumfk001@users.noreply.github.com>
2023-01-10 17:34:30 +0000

    Create grid based on function input

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const grid = document.querySelector('.grid');
+const gridSize = 500;
 
 
 function createGrid (userInput){
@@ -8,11 +9,12 @@ for (let i = 0; i < userInput**2; i++){
 
 let pixels = document.createElement('div');
 pixels.classList.add('pixel');
+pixels.style.height = gridSize/userInput + 'px';
+pixels.style.width = gridSize/userInput + 'px';
 grid.appendChild(pixels);
 }
 };
 
 createGrid(16);
 
-console.log(500/16);
 
